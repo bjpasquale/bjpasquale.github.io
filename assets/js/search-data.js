9e8b820ce912b9259eba06114b1d3ebedb897004
bjpasquale <bjpasquale@gmail.com>
2026-02-12 18:27:58 +0000

    Deploying to gh-pages from @ bjpasquale/bjpasquale.github.io@b2a9847ddc260fe44cf9de438a432b0d2def077e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -459,7 +459,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=7DQN8ksAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
